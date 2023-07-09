2b14d60b4f7fd3458ebc411f0102faf04158803e
João Vitor S <jvsg27@hotmail.com>
2023-07-08 22:26:47 -0300

    JS e funcionabilidade

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const jogadores =  document.querySelectorAll('.jogador')
+
+jogadores.forEach(personagem => {
+    personagem.addEventListener('mouseenter',() => {
+       
+        const personagemselecionado = document.querySelector('.selecionado');
+        personagemselecionado.classList.remove('selecionado')
+
+        personagem.classList.add('selecionado')
+
+        const imgpersonagemgrande = document.querySelector('.jogador-grande')
+
+        const imaperso = personagem.getAttribute('data-ima')
+        imgpersonagemgrande.src = `./src/imagens/${imaperso}`
+
+        const nomeperso = document.getElementById('nome-jogador')
+        nomeperso.innerText = personagem.getAttribute('data-name')
+
+        const descriperso = document.getElementById('descricao-jogador')
+        descriperso.innerText = personagem.getAttribute('data-descri')
+    } )
+})
